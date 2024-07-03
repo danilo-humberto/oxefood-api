7956627005211bd64afd6eeac2b881dfaec08562
aluno <aluno@ifpe.edu.br>
2024-07-03 17:59:45 -0300

    add: adicionando funcao delete

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ifpe.oxefood.api.cliente.ClienteRequest;
 import br.com.ifpe.oxefood.model.entregador.Entregador;
 import br.com.ifpe.oxefood.model.entregador.EntregadorService;
 
@@ -48,5 +48,12 @@ public class EntregadorController {
 
     entregadorService.update(id, request.build());
     return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        entregadorService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
