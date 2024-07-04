24411804dc8cac12b25c4b8423958448d67ddbc1
aluno <aluno@ifpe.edu.br>
2024-07-04 17:12:11 -0300

    add: adicionando classes de fornecedor

---
+++ src/main/java/br/com/ifpe/oxefood/model/fornecedor/Fornecedor.java
@@ -0,0 +1,45 @@
+package br.com.ifpe.oxefood.model.fornecedor;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.SQLRestriction;
+
+import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Fornecedor")
+@SQLRestriction("habilitado = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Fornecedor extends EntidadeAuditavel{
+
+    @Column(length = 100)
+    private String nome;
+
+    @Column
+    private String endereco;
+
+    @Column(length = 20)
+    private LocalDate dataFundacao;
+
+    @Column
+    private Double valorMercado;
+
+    @Column
+    private String paginaWeb;
+
+    @Column
+    private String contatoVendedor;
+    
+}
